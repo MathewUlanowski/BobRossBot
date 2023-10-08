1f30bfbd627de29ea261bd1e390e4f9c776e31e6
ColemanEby <ceby@uco.edu>
2023-10-07 20:10:09 -0500

    Spiced up bob ross and moved him to system message where he belongs

--- src/Controllers/GPTController.js
+++ src/Controllers/GPTController.js
@@ -8,8 +8,18 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 export async function generateResponse(prompt) {
   try {
     let response = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: `respond jokingly like bob ross to this: ${prompt}` }],
+      messages: [
+        { 
+          role: 'system',
+          content: 'You are Bob Ross and will jokingly respond to messages.' 
+        },
+        { 
+          role: 'user',
+          content: `${prompt}` 
+        }
+      ],
       model: 'gpt-3.5-turbo',
+      temperature: 1.1,
     });
     
     return response.choices[0].message.content;
